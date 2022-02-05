54e5beb74964fc89bdc49c5875611b7de7000682
Jarryd Cheso <jarryd.cheso@gmail.com>
2022-02-05 18:24:00 +0000

    add call logic and query parsing

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const url = require("url");
+const axios = require("axios");
+
 require("dotenv").config();
 
 router.get("/ping", async (req, res) => {
@@ -7,42 +10,52 @@ router.get("/ping", async (req, res) => {
 });
 
 router.post("/call", async (req, res) => {
-  const axios = require("axios");
-  TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
-  TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
-  TWILIO_NUMBER = process.env.TWILIO_NUMBER;
+  const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
+  const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+  const TWILIO_NUMBER = process.env.TWILIO_NUMBER;
   const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
-  // Parse the request body for params
+  const query = url.parse(req.url, true).query;
+  const origin = query.origin;
+  const destination = query.destination;
+  const timeBeforeAlarm = query.timeBeforeAlarm;
+  const deviceTime = query.deviceTime;
 
-  const params = { key: GOOGLE_API_KEY };
+  let code = 0;
+
+  const params = {
+    origin: origin,
+    destination: destination,
+    key: GOOGLE_API_KEY,
+  };
+
   let response = {};
+
   try {
-    console.log(params.key);
     response = await axios.get(
-      "https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal",
+      "https://maps.googleapis.com/maps/api/directions/json?",
       { params }
     );
-    console.log(response);
     timeToDestination = response.data.routes[0].legs[0].duration.value;
+    // ! Adjust based off device time
+    if (timeToDestination <= timeBeforeAlarm) {
+      const client = require("twilio")(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+      console.log(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+      client.calls
+        .create({
+          twiml: "<Response><Say>Ahoy there!</Say></Response>",
+          to: "+447801824101",
+          from: `${TWILIO_NUMBER}`,
+        })
+        .then((call) => console.log(call.sid));
+      console.log("RING RING BITCHES");
+    }
+    code = 200;
   } catch (error) {
     console.log(error);
+    code = 404;
   }
-
-  console.log(timeToDestination);
-  // Logic for calling
-
-  // ! TWILIO CALL
-  // const client = require("twilio")(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
-  // console.log(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
-  // client.calls
-  //   .create({
-  //     twiml: "<Response><Say>Ahoy there!</Say></Response>",
-  //     to: "+447801824101",
-  //     from: `${TWILIO_NUMBER}`,
-  //   })
-  //   .then((call) => console.log(call.sid));
-  return res.send(timeToDestination.toString());
+  return res.send(code);
 });
 
 module.exports = router;
