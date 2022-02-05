f1cc559379ac3046092c439decb741d9b6608544
Jarryd Cheso <jarryd.cheso@gmail.com>
2022-02-05 16:54:13 +0000

    bootstrap node server and add twilio + google api calls

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const Router = require("./routes");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+// ** Middleware
+// defining the Express app
+const app = express();
+// adding Helmet to enhance your API's security
+app.use(helmet());
+// using bodyParser to parse JSON bodies into JS objects
+app.use(bodyParser.json());
+// enabling CORS for all requests
+app.use(cors());
+// adding morgan to log HTTP requests
+app.use(morgan("tiny"));
+// defining an endpoint to return all ads
+app.use(Router);
+
+// starting the server
+app.listen(3001, () => {
+  console.log("listening on port 3001");
+});
---
+++ src/routes/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+require("dotenv").config();
+
+router.get("/ping", async (req, res) => {
+  return res.sendStatus(200);
+});
+
+router.get("/call", async (req, res) => {
+  const axios = require("axios");
+  TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
+  TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+  TWILIO_NUMBER = process.env.TWILIO_NUMBER;
+  const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+
+  // Parse the request body for params
+
+  const params = { key: GOOGLE_API_KEY };
+  const directions = await axios.get(
+    "https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal",
+    { params }
+  );
+
+  console.log(directions.data.routes.legs);
+
+  // ! TWILIO CALL
+  // const client = require("twilio")(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+  // console.log(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+  // client.calls
+  //   .create({
+  //     twiml: "<Response><Say>Ahoy there!</Say></Response>",
+  //     to: "+447801824101",
+  //     from: `${TWILIO_NUMBER}`,
+  //   })
+  //   .then((call) => console.log(call.sid));
+
+  return res.sendStatus(200);
+});
+
+module.exports = router;
