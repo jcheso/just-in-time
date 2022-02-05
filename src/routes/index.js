732a43f2f4243a3e74003aab62dad07b9f7b9c60
Jarryd Cheso <jarryd.cheso@gmail.com>
2022-02-05 23:21:01 +0000

    add no answer callback

--- src/routes/index.js
+++ src/routes/index.js
@@ -21,7 +21,9 @@ router.post("/call", async (req, res) => {
   const speed = query.speed;
   const deviceTime = query.deviceTime;
   const timeBeforeAlarm = query.timeBeforeAlarm;
+  // const safetyPhoneNumber = query.safetyPhoneNumber;
 
+  // Get longlat coordinates from origin and destination input
   const originArray = origin.split(",");
   const destinationArray = destination.split(",");
   const originLat = originArray[0];
@@ -57,15 +59,59 @@ router.post("/call", async (req, res) => {
       Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     const d = R * c; // in metres
-
+    const straightLineDurationToDestination = d / speed;
     if (straightLineDurationToDestination <= timeBeforeAlarm) {
+      const num = "+447801824101";
+      const safetyPhoneNumber = "+447762711868";
+      const count = 0;
+
       console.log("RING RING BITCHES");
+      const client = require("twilio")(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+      client.calls
+        .create({
+          twiml: "<Response><Say>Ahoy there!</Say></Response>",
+          to: num,
+          from: `${TWILIO_NUMBER}`,
+          statusCallback: `https:///https://ichack-22.herokuapp.com/no-answer?number=${num}&count=${count}`,
+          statusCallbackEvent: ["no-answer"],
+          statusCallbackMethod: "POST",
+        })
+        .then((call) => console.log(call.status));
+    }
+    statusCode = 200;
+  } catch (error) {
+    console.log(error);
+    statusCode = 404;
+  }
+  return res.sendStatus(statusCode);
+});
+
+router.post("/no-answer", async (req, res) => {
+  const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
+  const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+  const TWILIO_NUMBER = process.env.TWILIO_NUMBER;
+  const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+
+  const query = url.parse(req.url, true).query;
+  const count = query.count + 1;
+  const num = query.number;
+
+  console.log("NO ANSWER");
+  console.log("COUNT: " + count);
+
+  try {
+    //Get count from query string
+    if (count < 3) {
+      console.log("RING RING BITCHES AGAIN");
       const client = require("twilio")(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
       client.calls
         .create({
           twiml: "<Response><Say>Ahoy there!</Say></Response>",
           to: "+447801824101",
           from: `${TWILIO_NUMBER}`,
+          statusCallback: `https:///https://ichack-22.herokuapp.com/no-answer?number=${num}&count=${count}`,
+          statusCallbackEvent: ["no-answer"],
+          statusCallbackMethod: "POST",
         })
         .then((call) => console.log(call.status));
     }
